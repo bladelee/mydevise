26af103dec5cdf5c31601113ed22df0a75b4c391
bladelee <bj_liwei@yahoo.com>
2014-05-27 21:42:11 +0800

    add unicorn start script

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/home/bladelee/project/mydevise"
+pid "/home/bladelee/project/mydevise" + "/tmp/pids/unicorn.pid"
+stderr_path "/home/bladelee/project/mydevise" + "/log/unicorn.log"
+stdout_path "/home/bladelee/project/mydevise" + "/log/unicorn.log"
+
+listen "/tmp/unicorn.mydevise.sock"
+worker_processes 2
+timeout 30
---
+++ script/start.sh
@@ -0,0 +1,6 @@
+
+cd mydevise
+unicorn -D -c config/unicorn.rb
+
+sudo /etc/init.d/nginx start
+
