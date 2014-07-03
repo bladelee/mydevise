5f7274ae69158ae1553abcb944cbe9b0dcace27d
vagrant <vagrant@precise32.(none)>
2014-07-03 15:38:25 +0000

    merge all changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+
 Mydevise::Application.routes.draw do
   devise_for :sdkusers
 
@@ -60,3 +61,4 @@ Mydevise::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 end
+
